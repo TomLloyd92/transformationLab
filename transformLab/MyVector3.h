207eaca83480079662a29b089788231aa333a6ca
(Student) - Thomas Lloyd <c00231308@ITCARLOW.IE>
2018-12-13 13:04:39 +0000

    Added sfml

--- transformLab/MyVector3.h
+++ transformLab/MyVector3.h
@@ -1,6 +1,23 @@
 #ifndef MYVECTOR3 
 #define MYVECTOR3
 
+#ifdef _DEBUG  
+#pragma comment(lib,"sfml-graphics-d.lib")
+#pragma comment(lib,"sfml-audio-d.lib")
+#pragma comment(lib,"sfml-system-d.lib")
+#pragma comment(lib,"sfml-window-d.lib")
+#pragma comment(lib,"sfml-network-d.lib")
+#else
+#pragma comment(lib,"sfml-graphics.lib")
+#pragma comment(lib,"sfml-audio.lib")
+#pragma comment(lib,"sfml-system.lib")
+#pragma comment(lib,"sfml-window.lib")
+#pragma comment(lib,"sfml-network.lib")
+#endif
+#pragma comment(lib,"opengl32.lib")
+#pragma comment(lib,"glu32.lib")  
+
+
 
 /// <summary>
 /// your name here or no marks
